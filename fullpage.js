87d6c0f77b8e6aeefc864aadf1420bc2d1413c2d
Naman Chandok <42838783+NamanChandok@users.noreply.github.com>
2020-08-12 13:26:56 +0530

    And cut through all this red tape

--- fullpage.js
+++ fullpage.js
@@ -29,7 +29,7 @@
     var WRAPPER =               'fullpage-wrapper';
     var WRAPPER_SEL =           '.' + WRAPPER;
 
-    // slimscroll
+    // slimscroll 
     var SCROLLABLE =            'fp-scrollable';
     var SCROLLABLE_SEL =        '.' + SCROLLABLE;
 
