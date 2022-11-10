a29b0ebc6650470e7e9d95126adfd2557cacc80b
Naman Chandok <namanchandok1@gmail.com>
2022-11-10 14:15:04 +0530

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -25,7 +25,7 @@ export default function About({}: Props) {
             }}
             viewport={{once:true}}
             src="./hello.jpg"
-            className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover object-center md:rounded-lg md:w-64 md: h-95 xl:w-[300px] xl:h-[400px]'
+            className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover object-left md:rounded-lg md:w-64 md: h-95 xl:w-[300px] xl:h-[400px]'
             />
         <motion.div
             initial={{
