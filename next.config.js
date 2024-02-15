32129cd206ce9eb588159cafd23c96c8257cae97
Sharan Suri <31404638+Sharan420@users.noreply.github.com>
2024-02-15 20:26:00 +0530

    test

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: "/nextjs-github-pages",
+  images: {
+    unoptimized: true,
+  },
 }
  
 module.exports = nextConfig
