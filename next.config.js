7d1100736e9247d9c7bc146fb263bde76dffc90c
Naman <namanchandok1@gmail.com>
2024-02-15 19:48:27 +0530

    gh pages

--- next.config.js
+++ next.config.js
@@ -1,17 +1,12 @@
-const isGithubActions = process.env.GITHUB_ACTIONS || false
+/**
+* @type {import('next').NextConfig}
+*/
+const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix: './',
+};
 
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-}
+export default nextConfig;
