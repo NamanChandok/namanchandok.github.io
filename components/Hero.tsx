e73e43420c7fcaa4183364093d8ca549ca798c88
Naman Chandok <namanchandok1@gmail.com>
2025-05-06 19:53:36 +0530

    Update professional profile and skills

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,106 +1,132 @@
-import React from 'react'
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import Background from "./Background";
-import {motion} from 'framer-motion';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFileContract } from '@fortawesome/free-solid-svg-icons';
+import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFileContract } from "@fortawesome/free-solid-svg-icons";
 
-type Props = {}
+type Props = {};
 
 export default function Hero({}: Props) {
   const [text, count] = useTypewriter({
     words: [
-      "Front-End Developer",
+      "Full Stack Developer",
+      "Machine Learning Enthusiast",
       "Adobe Connoisseur",
       "Coffee Addict",
-      "brain the just not working"
+      "brain the just not working",
     ],
     loop: true,
-    delaySpeed: 4000
-  });  
+    delaySpeed: 4000,
+  });
   return (
-    <div className='h-screen flex flex-col justify-center relative items-center text-center overflow-hidden bg-[#111]'>
+    <div className="h-screen flex flex-col justify-center relative items-center text-center overflow-hidden bg-[#111]">
       <Background />
       <div className="absolute bg-bruh-black/50 inset-0 z-0"></div>
-      <div className='z-20 space-y-3'>
+      <div className="z-20 space-y-3">
         <div>
-          <motion.h2 initial={{
-              clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)',
+          <motion.h2
+            initial={{
+              clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
             }}
             animate={{
-              clipPath: 'polygon(0px 100%, 100% 100%, 100% 0px, 0px 0px)'
+              clipPath: "polygon(0px 100%, 100% 100%, 100% 0px, 0px 0px)",
             }}
             transition={{
-              duration:2.5, 
-              ease:'anticipate',
+              duration: 2.5,
+              ease: "anticipate",
               delay: 0.5,
-            }} className='text-lg uppercase text-gray-400 opacity-70 tracking-[5px]'>
+            }}
+            className="text-lg uppercase text-gray-400 opacity-70 tracking-[5px]"
+          >
             Hi there, I&apos;m
           </motion.h2>
-          <motion.h1 initial={{
-              clipPath: 'polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)',
+          <motion.h1
+            initial={{
+              clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
             }}
             animate={{
-              clipPath: 'polygon(0px 100%, 100% 100%, 100% 0px, 0px 0px)'
+              clipPath: "polygon(0px 100%, 100% 100%, 100% 0px, 0px 0px)",
             }}
             transition={{
-              duration:2.5, 
-              ease:'anticipate',
+              duration: 2.5,
+              ease: "anticipate",
               delay: 0.5,
-            }} className='text-3xl lg:text-6xl text-bruh-white font-semibold font-akira p-2 px-10'>
+            }}
+            className="text-3xl lg:text-6xl text-bruh-white font-semibold font-akira p-2 px-10"
+          >
             Naman Chandok
           </motion.h1>
-          <motion.p initial={{
-              opacity:0,
-              y:20
-            }}
-            animate={{
-              opacity:1,
-              y:0
-            }}
-            transition={{
-              duration:2.5, 
-              ease:'anticipate',
-              delay: 1,
-            }} className='text-lg text-gray-400 opacity-70 pb-4'>
-            <span>{text}</span>
-            <Cursor cursorColor='#7c6aab' />
-          </motion.p>
-          <motion.a target='_blank' 
-            href='https://docs.google.com/document/d/14uKJW6Xv5lxFtsOCv4H1ggWbsm3HZOSQpp7KCxWiIwE/edit?usp=sharing' 
+          <motion.p
             initial={{
-              opacity:0,
+              opacity: 0,
+              y: 20,
             }}
             animate={{
-              opacity:1,
+              opacity: 1,
+              y: 0,
             }}
             transition={{
-              duration:2, 
-              ease:'anticipate',
+              duration: 2.5,
+              ease: "anticipate",
+              delay: 1,
+            }}
+            className="text-lg text-gray-400 opacity-70 pb-4"
+          >
+            <span>{text}</span>
+            <Cursor cursorColor="#7c6aab" />
+          </motion.p>
+          <motion.a
+            target="_blank"
+            href="https://docs.google.com/document/d/14uKJW6Xv5lxFtsOCv4H1ggWbsm3HZOSQpp7KCxWiIwE/edit?usp=sharing"
+            initial={{
+              opacity: 0,
+            }}
+            animate={{
+              opacity: 1,
+            }}
+            transition={{
+              duration: 2,
+              ease: "anticipate",
               delay: 1.5,
-            }} className='text-gray-400 border-2 border-gray-400 hover:border-gray-300 py-1.5 px-4 rounded-full opacity-70 hover:text-gray-300 transition-all duration-300 ease-in-out'>
-            <FontAwesomeIcon icon={faFileContract} className='h-4 pr-1 inline-block' /> View Resume
+            }}
+            className="text-gray-400 border-2 border-gray-400 hover:border-gray-300 py-1.5 px-4 rounded-full opacity-70 hover:text-gray-300 transition-all duration-300 ease-in-out"
+          >
+            <FontAwesomeIcon
+              icon={faFileContract}
+              className="h-4 pr-1 inline-block"
+            />{" "}
+            View Resume
           </motion.a>
         </div>
       </div>
-      <motion.div initial={{
-              bottom:'-20vh',
-            }}
-            animate={{
-              bottom:0,
-            }}
-            transition={{
-              duration:2, 
-              ease:'anticipate',
-              delay: 1.5,
-            }} className="custom-shape-divider-bottom-1687952567 heroTexture z-0">
-        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+      <motion.div
+        initial={{
+          bottom: "-20vh",
+        }}
+        animate={{
+          bottom: 0,
+        }}
+        transition={{
+          duration: 2,
+          ease: "anticipate",
+          delay: 1.5,
+        }}
+        className="custom-shape-divider-bottom-1687952567 heroTexture z-0"
+      >
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
           <path
             d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
-            className="shape-fill"></path>
+            className="shape-fill"
+          ></path>
         </svg>
       </motion.div>
     </div>
-  )
+  );
 }
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,42 +1,41 @@
-import React from 'react';
-import {motion} from "framer-motion";
-import Skill from './Skill'
+import React from "react";
+import { motion } from "framer-motion";
+import Skill from "./Skill";
 
-type Props = {}
+type Props = {};
 
 export default function Skills({}: Props) {
   return (
-    <motion.div 
-        initial={{opacity:0}}
-        whileInView={{opacity:1}}
-        transition={{duration:1}}
-        className='flex relative flex-col text-center my-auto md:text-left xl:px-10 items-center mx-auto justify-center py-16'>
-        
-        <h3 className='text-5xl font-semibold font-akira'>Skills</h3>
-        <div className='grid grid-cols-3 md:grid-cols-6 gap-5 mt-10'>
-            <Skill imgUrl="./skills/html.png" name='html' />
-            <Skill imgUrl="./skills/css.png" name='css' />
-            <Skill imgUrl="./skills/js.png" name='javascript' />
-            <Skill imgUrl="./skills/ts.png" name='typescript' />
-            <Skill imgUrl="./skills/ps.png" name='photoshop' />
-            <Skill imgUrl="./skills/ae.png" name='after effects' />
-            <Skill imgUrl="./skills/pr.png" name='premier pro' />
-            <Skill imgUrl="./skills/ai.png" name='graphic design' />
-            <Skill imgUrl="./skills/xd.png" name='ui design' />
-            <Skill imgUrl="./skills/3d.png" name='3d modelling' />
-            <Skill imgUrl="./skills/py.png" name='python' />
-            <Skill imgUrl="./skills/php.png" name='php' />
-            <Skill imgUrl="./skills/react.png" name='react.js' />
-            <Skill imgUrl="./skills/next.png" name='next.js' />
-            <Skill imgUrl="./skills/flask.png" name='flask' />
-            <Skill imgUrl="./skills/firebase.png" name='firebase' />
-            <Skill imgUrl="./skills/git.png" name='git' />
-            <Skill imgUrl="./skills/tailwind.png" name='tailwind css' />
-            <Skill imgUrl="./skills/heroku.png" name='heroku' />
-            <Skill imgUrl="./skills/mysql.png" name='mysql' />
-            <Skill imgUrl="./skills/bs.png" name='bootstrap' />
-        </div>
-
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      className="flex relative flex-col text-center my-auto md:text-left xl:px-10 items-center mx-auto justify-center py-16"
+    >
+      <h3 className="text-5xl font-semibold font-akira">Skills</h3>
+      <div className="grid grid-cols-3 md:grid-cols-6 gap-5 mt-10">
+        <Skill imgUrl="./skills/js.png" name="javascript" />
+        <Skill imgUrl="./skills/ts.png" name="typescript" />
+        <Skill imgUrl="./skills/py.png" name="python" />
+        <Skill imgUrl="./skills/mysql.png" name="sql" />
+        <Skill imgUrl="./skills/php.png" name="php" />
+        <Skill imgUrl="./skills/html.png" name="html" />
+        <Skill imgUrl="./skills/css.png" name="css" />
+        <Skill imgUrl="./skills/react.png" name="react.js" />
+        <Skill imgUrl="./skills/next.png" name="next.js" />
+        <Skill imgUrl="./skills/node.png" name="node.js" />
+        <Skill imgUrl="./skills/flask.png" name="flask" />
+        <Skill imgUrl="./skills/firebase.png" name="firebase" />
+        <Skill imgUrl="./skills/mongodb.png" name="mongodb" />
+        <Skill imgUrl="./skills/supabase.png" name="supabase" />
+        <Skill imgUrl="./skills/git.png" name="git" />
+        <Skill imgUrl="./skills/tailwind.png" name="tailwind css" />
+        <Skill imgUrl="./skills/ps.png" name="photoshop" />
+        <Skill imgUrl="./skills/ae.png" name="after effects" />
+        <Skill imgUrl="./skills/ai.png" name="graphic design" />
+        <Skill imgUrl="./skills/ui.png" name="ui design" />
+        <Skill imgUrl="./skills/3d.png" name="3d modelling" />
+      </div>
     </motion.div>
-  )
+  );
 }
