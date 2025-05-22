6c061be706066f0f4790e147416078191973fe9a
Naman Chandok <namanchandok1@gmail.com>
2025-05-22 14:17:25 +0530

    Update Project.tsx

--- components/Project.tsx
+++ components/Project.tsx
@@ -13,16 +13,14 @@ export default function Project({ title, imgUrl, desc, url }: Props) {
   return (
     <Link
       href={url}
-      className="flex-shrink-0 relative snap-center flex flex-col items-center bg-muted/30 group rounded-xl overflow-hidden"
+      className="bg-muted/30 p-1 group rounded-xl"
       aria-label={title}
     >
-      <div className="h-64 w-full p-1">
-        <div
-          className="h-full w-full rounded-lg bg-cover grayscale-50 opacity-70 group-hover:grayscale-0 group-hover:opacity-100 transition duration-300"
-          style={{ backgroundImage: `url(${imgUrl}` }}
-        />
-      </div>
-      <div className="flex flex-col justify-end space-y-1 p-3 pt-1 w-full">
+      <div
+        className="h-56 w-full rounded-lg bg-cover grayscale-50 opacity-70 group-hover:grayscale-0 group-hover:opacity-100 transition duration-300"
+        style={{ backgroundImage: `url(${imgUrl}` }}
+      />
+      <div className="space-y-1 p-3 pt-2">
         <h4 className="text-xl font-semibold text-foreground">{title}</h4>
         <div className="relative h-6 overflow-hidden">
           <p className="text-secondary absolute translate-y-0 transition duration-300 group-hover:-translate-y-10">
