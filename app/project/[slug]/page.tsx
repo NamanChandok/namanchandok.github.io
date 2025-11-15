ea07a52fb63d6ef1e7c0425c3758a454da472881
Naman Chandok <namanchandok1@gmail.com>
2025-11-16 00:02:37 +0530

    Add generateStaticParams for project pages

--- app/project/[slug]/page.tsx
+++ app/project/[slug]/page.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 import ProjectComponent from "../../../components/ProjectPage";
+import { projectsData } from "../../../components/projectsdata";
 
 interface ProjProps {
   params: {
     slug: string;
   };
 }
+
+const project_slugs = projectsData.map((project) => ({
+  slug: project.title
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^a-z0-9\-]/g, ""),
+}));
+
+export const generateStaticParams = async () => {
+  return project_slugs;
+};
 
 const Proj: React.FC<ProjProps> = async ({ params }) => {
   const { slug } = await params;
